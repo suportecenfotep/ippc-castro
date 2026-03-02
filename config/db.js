6bb1276991211d78d10d313f948af2ab44ff25f1
José D. António <suportecenfotep@gmail.com>
2026-03-02 23:46:13 +0100

    Novo commit

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 const { Sequelize } = require("sequelize");
 
-// const conn = new Sequelize('bfq8wtsefytinjkflkoj', 'u4dhnuitxfytovrs', 'fFHf0XoCDGxbZEUHaGbu', {
-//     host: 'bfq8wtsefytinjkflkoj-mysql.services.clever-cloud.com',
-//     dialect: 'mysql'
-// });
-
-const conn = new Sequelize('cenfolite_escolar', 'root', '', {
-    host: 'localhost',
+const conn = new Sequelize('bfq8wtsefytinjkflkoj', 'u4dhnuitxfytovrs', 'fFHf0XoCDGxbZEUHaGbu', {
+    host: 'bfq8wtsefytinjkflkoj-mysql.services.clever-cloud.com',
     dialect: 'mysql'
 });
+
+// const conn = new Sequelize('cenfolite_escolar', 'root', '', {
+//     host: 'localhost',
+//     dialect: 'mysql'
+// });
 
 module.exports = conn;
